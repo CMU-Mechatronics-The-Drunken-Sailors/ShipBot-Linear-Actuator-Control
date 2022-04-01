145b9bccc53f7ef0237b0729437db2df0f2113d3
Brandon Joel Gonzalez <bgonz@sas.upenn.edu>
2022-04-01 18:36:51 -0400

    Update linear_actuator_test.ino

--- linear_actuator_test.ino
+++ linear_actuator_test.ino
@@ -2,32 +2,30 @@
 // Code to test three linear actuators using an Arduino Uno.
 // Uses this quad motor shield: https://wiki.dfrobot.com/Quad_Motor_Driver_Shield_for_Arduino_SKU_DRI0039
 
+// M1 controls the LA pair; M2 controls the solo LA
+
 const int M1 = 1;
 const int M2 = 2;
-const int M3 = 3;
-const int M4 = 4;
+// const int M3 = 3;
+// const int M4 = 4;
 
 const int M1_SPEED = 3;
 const int M2_SPEED = 11;
-const int M3_SPEED = 5;
-const int M4_SPEED = 6;
+// const int M3_SPEED = 5;
+// const int M4_SPEED = 6;
 
 const int M1_DIRECTION = 4;
 const int M2_DIRECTION = 12;
-const int M3_DIRECTION = 8;
-const int M4_DIRECTION = 7;
+// const int M3_DIRECTION = 8;
+// const int M4_DIRECTION = 7;
 
 void setup()
 {
   pinMode(M1_SPEED, OUTPUT);
   pinMode(M2_SPEED, OUTPUT);
-  pinMode(M3_SPEED, OUTPUT);
-  pinMode(M4_SPEED, OUTPUT);
   
   pinMode(M1_DIRECTION, OUTPUT);
   pinMode(M2_DIRECTION, OUTPUT);
-  pinMode(M3_DIRECTION, OUTPUT);
-  pinMode(M4_DIRECTION, OUTPUT);
 }
 
 void spin_motor(int motor, int speed)
@@ -60,31 +58,31 @@ void spin_motor(int motor, int speed)
       }
     break;
     
-    case 3:
-      if (speed < 0)
-      {
-        digitalWrite(M3_DIRECTION, HIGH); // go backwards
-        analogWrite(M3_SPEED, abs(speed)); // change speed
-      }
-      else
-      {
-        digitalWrite(M3_DIRECTION, LOW); // go forwards
-        analogWrite(M3_SPEED, speed); // change speed
-      }
-    break;
+    // case 3:
+    //   if (speed < 0)
+    //   {
+    //     digitalWrite(M3_DIRECTION, HIGH); // go backwards
+    //     analogWrite(M3_SPEED, abs(speed)); // change speed
+    //   }
+    //   else
+    //   {
+    //     digitalWrite(M3_DIRECTION, LOW); // go forwards
+    //     analogWrite(M3_SPEED, speed); // change speed
+    //   }
+    // break;
     
-    case 4:
-      if (speed > 0)
-      {
-        digitalWrite(M4_DIRECTION, HIGH); // go forwards
-        analogWrite(M4_SPEED, speed); // change speed
-      }
-      else
-      {
-        digitalWrite(M4_DIRECTION, LOW); // go backwards
-        analogWrite(M4_SPEED, abs(speed)); // change speed
-      }
-    break;
+    // case 4:
+    //   if (speed > 0)
+    //   {
+    //     digitalWrite(M4_DIRECTION, HIGH); // go forwards
+    //     analogWrite(M4_SPEED, speed); // change speed
+    //   }
+    //   else
+    //   {
+    //     digitalWrite(M4_DIRECTION, LOW); // go backwards
+    //     analogWrite(M4_SPEED, abs(speed)); // change speed
+    //   }
+    // break;
     
     default:
     break;
@@ -93,13 +91,13 @@ void spin_motor(int motor, int speed)
 
 void loop()
 {
-  spin_motor(M1, 100);
-  spin_motor(M2, 100);
-  spin_motor(M3, 100);
-  delay(1000);
+  // extend
+  spin_motor(M1, 255);
+  spin_motor(M2, 255);
+  delay(5000);
   
-  spin_motor(M1, -100);
-  spin_motor(M2, -100);
-  spin_motor(M3, -100);
-  delay(1000);
+  // retract
+  spin_motor(M1, -255);
+  spin_motor(M2, -255);
+  delay(5000);
 }
